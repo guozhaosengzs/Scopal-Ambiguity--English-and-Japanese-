9d2864d63f170ffc11df7ccd520b1861ce16165e
guozhaosengzs <guozhaosengzs@yahoo.com>
2021-04-05 20:38:54 -0400

    Updated Data and model
    Tried random-intercept only model

--- script.R
+++ script.R
@@ -15,14 +15,14 @@ SLenJap <- read_csv("data\\SentenceLengthJapanese.csv")
 ### Clean column names, check each of their type and change them if needed
 
 ## 2 of Length data sets
-uniformed.colnames <- c("subject", "sentence", "context", "sentence_length", "maxF0", "meanF0")
+uniformed.colnames <- c("subject", "sentence", "context", "sentence_length", "maxF0", "meanF0", "keyword")
 colnames(SLenEng) <- uniformed.colnames
 colnames(SLenJap) <- uniformed.colnames
 
 sapply(SLenEng, class)
 sapply(SLenJap, class)
 
-cols  <- c("subject", "sentence", "context")
+cols  <- c("subject", "sentence", "context", "keyword")
 SLenEng[cols] <- lapply(SLenEng[cols], as.factor)
 SLenJap[cols] <- lapply(SLenJap[cols], as.factor)
 
@@ -42,10 +42,19 @@ str(SLenJap)
 sapply(SLenEng, levels)
 
 #Random Slope and Random Intercept
-model.EngSent.Len <- lmer(sentence_length ~ context  + (1 + context|subject/sentence), data = SLenEng) 
- 
+model.EngSent.Len <- lmer(sentence_length ~ context  + (1|keyword)  + (1|subject), data = SLenEng)
 
+summary(model.EngSent.Len)
 
+model.EngSent.maxF0 <- lmer(maxF0 ~ context  + (1|keyword)  + (1|subject), data = SLenEng)
+
+summary(model.EngSent.maxF0)
+
+model.EngSent.meanF0 <- lmer(meanF0 ~ context  + (1|keyword)  + (1|subject), data = SLenEng)
+
+summary(model.EngSent.meanF0)
+
+model.EngSent.meanF0 <- lmer(meanF0 ~ (1|keyword) + (1|subject) + context, data = SLenEng)
 
 
 
